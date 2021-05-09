01f0e48a606e7a0bb9dcea2b7114628aeb109172
Cat Weird <52168620+catware1@users.noreply.github.com>
2021-05-09 23:07:57 +0300

    Create os-release.py

---
+++ configs/os-release.py
@@ -0,0 +1,10 @@
+
+#
+# Файл, идентичный /etc/os-release в GNU/Linux короче говоря. Устаревшая версия, актуальный находится в systeminformation/os-release.py
+#
+
+botname = "Имя бота"
+version = "'версия' бота"
+core = "0.9_51"
+abms_name = "Имя вашей ABMS, если вы хотите создать форк"
+codename = "Codename"
