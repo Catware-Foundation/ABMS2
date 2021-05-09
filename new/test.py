c99137509f004a128abe431e84a031d88b25fcbe
Cat Weird <52168620+catware1@users.noreply.github.com>
2021-05-09 23:14:27 +0300

    Create test.py

---
+++ new/test.py
@@ -0,0 +1,11 @@
+# *~* CatABMS Command *~* #
+#start-of-config
+trigger = "тест"
+description = "Тест бота и замер скорости работы"
+triggermode = "is"
+timeout = "10"
+author_pseudo = "Cat Weird"
+author_contacts = "t.me/catweir1"
+#end-of-config
+
+message("Бот жив.")
