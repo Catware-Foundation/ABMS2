f7cf81c610f6a21747df9dff25db212e9c7df4c2
Cat Weird <52168620+catware1@users.noreply.github.com>
2021-05-09 23:13:32 +0300

    Create pi.py

---
+++ new/pi.py
@@ -0,0 +1,12 @@
+# *~* CatABMS Command *~* #
+#start-of-config
+trigger = "пи"
+description = "Число Пи"
+triggermode = "is"
+timeout = "10"
+category = "Математика, Информационные"
+author_pseudo = "Boris Churikov (автор идеи)"
+author_contacts = "https://vk.com/spidfistashka"
+#end-of-config
+
+message(pi)
