59ca91a1a267baa21e54e8036137b6e27efbfc17
Cat Weird <52168620+catware1@users.noreply.github.com>
2021-05-09 23:10:44 +0300

    Create detectfull.py

---
+++ lib/detectfull.py
@@ -0,0 +1,19 @@
+
+#
+# Catware Detecting Full Size Image for VK_API
+#
+# Спизженно из Catinka (чесно)
+#
+# Возвращает URL большей картинки
+#
+# Требует обьект изображения (["attachments"][picture_index])
+#
+
+def detectfull(pictureobject):
+    maximum = 0
+    result = ""
+    for u in pictureobject["photo"]["sizes"]:
+        if u["height"]*u["width"] > maximum:
+            maximum = u["height"]*u["width"]
+            result = u["url"]
+    return result
